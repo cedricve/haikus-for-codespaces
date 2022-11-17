571d04a3547c0dbc10130ed41eeda28feeee1305
Cédric Verstraeten <cedric@verstraeten.io>
2022-11-17 10:40:35 +0000

    try this codespace feature

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
 
+console.log("hey yooo!")
 app.get('/', (req, res) => {
   res.render('index', {haikus: haikus});
 });
